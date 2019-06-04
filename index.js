8c20f56a282a1f594063c4b7499e3b745494cf12
Carly Pecora <carly@carlypecora.com>
2019-06-04 13:40:16 -0400

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(timeStr){
+	let hour = parseInt(timeStr);
+	if (hour < 12 && hour >= 0){
+		return "Good Morning";
+	} else if (hour >= 12 && hour <= 17){
+		return "Good Afternoon";
+	} else {
+		return "Good Evening";
+	}
+}
+
+function displayMessage(str) {
+	let greeting = document.querySelector("#greeting");
+	greeting.innerText = str;
+}
